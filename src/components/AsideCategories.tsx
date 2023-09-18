2fb0d9e8fe61a91a1f08a6c48639fe86afd0eefb
Lilly <nuralievalilly17@gmail.com>
2023-09-18 18:18:19 +0300

    finished main slide

---
+++ src/components/AsideCategories.tsx
@@ -0,0 +1,30 @@
+'use client';
+import { categories } from '@/info/categories';
+import React, { useState } from 'react';
+import plusSvg from '../../public/assets/plus-solid 1.svg';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function AsideCategories() {
+  const [limit, setLimit] = useState(9);
+  return (
+    <div className={`home__main-categories ${limit > 10 ? 'scroll' : ''}`}>
+      {categories.slice(0, limit).map((category) => (
+        <Link href={`/${category}`} className='category' key={category}>
+          {category.split('-').join(' ')}
+        </Link>
+      ))}
+      {limit < 10 ? (
+        <button
+          onClick={() => setLimit(50)}
+          type='button'
+          className={`btn__show-more flex gap-x-3 ${limit > 10 ? 'scroll' : ''}`}>
+          <Image src={plusSvg} width={24} height={24} alt='show more' />
+          View all Category
+        </button>
+      ) : (
+        <button onClick={() => setLimit(9)}>Close</button>
+      )}
+    </div>
+  );
+}
