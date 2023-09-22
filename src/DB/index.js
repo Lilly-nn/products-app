188fded9be0a2188dc9649889490c764bcfede79
Lilly <nuralievalilly17@gmail.com>
2023-09-23 00:35:53 +0300

    modified db and changed password

--- src/DB/index.js
+++ src/DB/index.js
@@ -4,7 +4,7 @@ export async function connectToMongoDB() {
   try {
     mongoose
       .connect(
-        `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@products-app.vpsizlm.mongodb.net/?retryWrites=true&w=majority`
+        `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@products-app.y3hfsvv.mongodb.net/db?retryWrites=true&w=majority`
       )
       .then(() => console.log('connected to DB'));
   } catch (err) {
