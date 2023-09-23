1bfba7565c46737bc1e69e3eb67bae8e264b1eec
Lilly <nuralievalilly17@gmail.com>
2023-09-24 01:22:31 +0300

    made sign-in functionality

--- src/app/api/sign-in/route.ts
+++ src/app/api/sign-in/route.ts
@@ -1,7 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connectToMongoDB } from "../../../DB/index";
+import bcryptjs from "bcryptjs";
+import User from "@/DB/models/userModel";
+
+type RequestType = {
+    email: string;
+    password: string
+}
 
 export async function POST(request: NextRequest) {
     await connectToMongoDB();
+    const body: RequestType = await request.json();
+    try {
+        const validUser = await User.findOne({ email: body.email });
+        if (!validUser) {
+            return new NextResponse('not valid', {
+                status: 400,
+                statusText: "User credentials are not valid"
+            })
+        }
+        const validPassword = await bcryptjs.compare(body.password, validUser.password);
+        if (!validPassword) {
+            return new NextResponse('not valid password', {
+                status: 400,
+                statusText: "User credentials are not valid"
+            })
+        }
+        return new NextResponse("success", {
+            status: 200,
+            statusText: "User signed in"
+        })
+    } catch (err) {
+        console.log(err)
+    }
 
 }
